6078a0430e5bc87007557ed781165afc0ddf6155
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-22 08:16:05 -0400

    update

--- myapp/app/register/page.js
+++ myapp/app/register/page.js
@@ -4,6 +4,20 @@ const Register = () => {
   return (
     <section className='px-10 py-4x'>
           <h1 className="font-bold text-3xl md:text-5xl text-center mb-8 mt-8">Register</h1>
+          <div className='flex flex-col basis-full relative w-auto h-80 bg-wh-500 '>
+          <form className=' flex flex-col'>
+
+           <div className='flex flex-col m-auto' style={{maxWidth: "500px"}}>
+            <input type="text" placeholder="Email" className="input input-bordered input-xs w-full max-w-xs" />
+            <input type="text" placeholder="Username" className="input input-bordered input-xs w-full max-w-xs" />
+            <input type="text" placeholder="Password" className="input input-bordered input-xs w-full max-w-xs" />
+            <button className='btn'>Register</button>
+            </div>
+
+            </form>
+
+            
+          </div>
     </section>
   )
 }
