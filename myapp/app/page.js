3638615e7390b54474ed69ff1670b40ae727354c
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-22 22:12:56 -0400

    update

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <main className='flex-start flex-col'>
+   <main className='flex-start flex-col paddings'>
     <p>Feature</p>
     <h2>Post</h2>
    </main>
